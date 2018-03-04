82ed990841517baf14ed02f6777a80bd759bf692
FireController1847 <usfirepilot123@gmail.com>
2018-03-03 23:51:39 -0700

    Attempt To Add Participants DB

--- src/structures/Participant.js
+++ src/structures/Participant.js
@@ -1,9 +1,35 @@
+const fs = require('fs');
+
 class Participant {
   constructor(_id, name, color) {
     this._id = _id;
     this.name = name;
     this.color = color;
     this.room = null;
+
+    const pdb = this.requestFile();
+    if (!pdb) return;
+    if (pdb[this._id]) {
+      this.name = pdb[this._id].name;
+      this.color = pdb[this._id].color;
+    } else {
+      pdb[this._id] = this.generateJSON();
+      this.updateFile(pdb);
+    }
+  }
+  requestFile() {
+    try {
+      return JSON.parse(fs.readFileSync('../database/participants.json'));
+    } catch (e) {
+      return null;
+    }
+  }
+  updateFile(raw) {
+    try {
+      fs.writeFileSync('../database/participants.json', JSON.stringify(raw));
+    } catch (e) {
+      // ...
+    }
   }
   generateJSON() {
     return {
